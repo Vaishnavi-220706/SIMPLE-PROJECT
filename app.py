838fa466ac0ae404ca58d3fea0f41c9a6b7fd40e
Vaishnavi-220706 <pallaganivaishnavi123@gmail.com>
2025-08-02 12:10:40 +0530

    third update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "welcome to mrecw"
+    return render_template("index.html")
 @app.route("/home")
 def home():
     return "home page"
